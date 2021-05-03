4603a78d4e0e53182ca9c4017202b47de98a0f11
orow4727 <orow4727@uni.sydney.edu.au>
2021-05-03 15:56:20 +1000

    simple

--- lab5-audio/wsprcan/wsprd.c
+++ lab5-audio/wsprcan/wsprd.c
@@ -164,7 +164,7 @@ unsigned long readwavfile(char *ptr_to_infile, int ntrmin, double *idat, double 
     int ret = 1;
     int error;
 
-    if (!(s = pa_simple_new(NULL, argv[0], PA_STREAM_RECORD, NULL, "record", &ss, NULL, NULL, &error))) {
+    if (!(s = pa_simple_new(NULL, "wspr", PA_STREAM_RECORD, NULL, "record", &ss, NULL, NULL, &error))) {
         fprintf(stderr, __FILE__": pa_simple_new() failed: %s\n", pa_strerror(error));
     }
 
